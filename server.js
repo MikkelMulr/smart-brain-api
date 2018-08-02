6aac78e5faa1ba07b0b577ab13e8745ae95fd679
Mike.Mueller <MichaelM.Dev@gmail.com>
2018-08-02 14:13:06 -0400

    ready to start adding in the DB elements of the project

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -63,7 +65,6 @@ app.post('/register', (req, res) => {
     id: '125',
     name: name,
     email: email,
-    password: password,
     enteries: 0,
     joined: new Date()
   })
